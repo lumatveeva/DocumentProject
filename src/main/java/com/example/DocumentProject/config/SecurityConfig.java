f7c070808309a90a8bb897e0491f8130c07ff915
IKharipov <ilnurkharipov@gmail.com>
2024-05-16 23:24:06 +0500

    Добавлен SecurityConfig.java

---
+++ src/main/java/com/example/DocumentProject/config/SecurityConfig.java
@@ -0,0 +1,7 @@
+package com.example.DocumentProject.config;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+public class SecurityConfig  {
+}
